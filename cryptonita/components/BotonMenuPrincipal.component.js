ed0c1ed737dcc5d3c715c0b54d2af1dcfbb32bd5
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2022-02-12 11:30:06 -0500

    Agregué varias imagenes, primera fase de paginas 1)index, 2) termCondi, 3) nosotros. Instalé react-boostrap

---
+++ cryptonita/components/BotonMenuPrincipal.component.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function BotonMenuPrincipal() 
+{
+    
+    return (
+    <Link href="/">
+        <button className="regresar">
+            Menú Principal
+        </button>
+    </Link>
+    ); 
+    
+}
---
+++ cryptonita/pages/nosotros.js
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import BotonMenuPrincipal from "../components/botonMenuPrincipal.component";
+
+/*
+NFTs recuperados de: https://thedogepoundnft.com/doge#
+*/
+export default function nosotros() 
+{
+    return <>
+        <h1>Nosotros</h1>
+        <p>El equipo detrás de este proyecto</p>
+        <div className="persona">
+            <div className="columnaImagen">
+                <img src="/dodge-fino.png"/>
+            </div>
+            <div>
+                <p>Sebastián Chávarry</p>
+                <p>Sárcastico, pero buena gente</p>
+                <a href="https://www.linkedin.com/in/sebasti%C3%A1n-ch%C3%A1varry-gutierrez/">LinkedIn</a>
+                {' '}
+                <a href="https://github.com/sebastianperudev2001">GitHub</a>
+            </div>
+        </div>
+
+        <div className="persona">
+            <div className="columnaImagen">
+                <img src="/dodge-loco.png"/>
+            </div>
+            <div>
+                <p>Sebastián Chávarry</p>
+                <p>Sárcastico, pero buena gente</p>
+                <a href="https://www.linkedin.com/in/sebasti%C3%A1n-ch%C3%A1varry-gutierrez/">LinkedIn</a>
+                {' '}
+                <a href="https://github.com/sebastianperudev2001">GitHub</a>
+            </div>
+        </div>
+
+        <div className="persona">
+            <div className="columnaImagen">
+                <img src="/dodge-pizza.png"/>
+            </div>
+            <div>
+                <p>Sebastián Chávarry</p>
+                <p>Sárcastico, pero buena gente</p>
+                <a href="https://www.linkedin.com/in/sebasti%C3%A1n-ch%C3%A1varry-gutierrez/">LinkedIn</a>
+                {' '}
+                <a href="https://github.com/sebastianperudev2001">GitHub</a>
+            </div>
+        </div>
+
+        <div className="persona">
+            <div className="columnaImagen">
+                <img src="/dodge-fiesta.png"/>
+            </div>
+            <div>
+                <p>Sebastián Chávarry</p>
+                <p>Sárcastico, pero buena gente</p>
+                <a href="https://www.linkedin.com/in/sebasti%C3%A1n-ch%C3%A1varry-gutierrez/">LinkedIn</a>
+                {' '}
+                <a href="https://github.com/sebastianperudev2001">GitHub</a>
+            </div>
+        </div>
+        <BotonMenuPrincipal/> 
+
+    </>
+}
