6dbee97720bb45166e7da2d398dc14c2dd5eb5e5
Matias Villanueva <matias.villanueva.paz@gmail.com>
2022-02-16 22:17:03 -0500

    HistorialOperaciones (Pruebas con backEnd)

--- cryptonita/components/Historial-operaciones.js
+++ cryptonita/components/Historial-operaciones.js
@@ -1,20 +1,31 @@
-const HisotrialOperaciones = () =>{
+const HisotrialOperaciones = (props) => {
     return <main className="col-md-8">
-    <h3 className="mt-4">Ranking</h3>
-    <table className="table mt-4    ">
-        <thead>
-            <tr>
-                <th>ID de Operación</th>
-                <th>Fecha y Hora</th>
-                <th>Tipo de Operación</th>
-                <th>Tipo de Cambio</th>
-                <th>Monto</th>
-            </tr>
-        </thead>
-        <tbody id="data_proyectos">
-            
-        </tbody>
-    </table>
-</main>
+        <h3 className="mt-4">Historial de Operaciones</h3>
+        <table className="table mt-4 table-dark">
+            <thead>
+                <tr>
+                    <th>ID de Operación</th>
+                    <th>Fecha y Hora</th>
+                    <th>Tipo de Operación</th>
+                    <th>Tipo de Cambio</th>
+                    <th>Monto</th>
+                </tr>
+            </thead>
+            <tbody id="data_operaciones">
+                {
+                    props.Operaciones.map((operacion) => {
+                        return <tr key={operacion.id}>
+                            <td>{operacion.id}</td>
+                            <td>{operacion.FechaHora}</td>
+                            <td>{operacion.Tipo}</td>
+                            <td>{operacion.TipoCamio}</td>
+                            <td>{operacion.Monto}</td>
+
+                        </tr>
+                    })
+                }
+            </tbody>
+        </table>
+    </main>
 }
 export default HisotrialOperaciones
