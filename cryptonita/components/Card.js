22a40f59b80822f440618841eb7fab42a9e571af
Matias Villanueva <matias.villanueva.paz@gmail.com>
2022-02-16 20:08:25 -0500

    Creación de historia 35

--- cryptonita/components/Card.js
+++ cryptonita/components/Card.js
@@ -1,5 +1,22 @@
+import { useState } from "react"
 const Card = (props) => {
 
+    const [NumeroCuenta, setNumeroCuenta] = useState("")
+    const [NumeroBilletera, setNumeroBilletera] = useState("")
+    const [NumeroTransaccion, setNumeroTransaccion] = useState("")
+
+    const NumeroCuentaOnChange = (event) => {
+        setNumeroCuenta(event.target.value)
+        console.log(NumeroCuenta)
+    }
+    const NumeroBilleteraOnChange = (event) => {
+        setNumeroBilletera(event.target.value)
+        console.log(NumeroBilletera)
+    }
+    const NumeroTransaccionOnChange = (event) => {
+        setNumeroTransaccion(event.target.value)
+        console.log(NumeroTransaccion)
+    }
 
     if (props.modo == "H28") {
         return <aside >
@@ -8,14 +25,14 @@ const Card = (props) => {
                     <h3 className="card-title text-center">Venta de Bitcoins</h3>
                     <h4 className="text-center">Paso 1 de 3</h4>
                     <h5 className="card-subtitle mb-4 mt-4 text-muted">Ingrese su numero de cuenta: </h5>
-                    <input type="text" className="form-control mt-4" />
+                    <input type="text" className="form-control mt-4" defaultValue={ NumeroCuenta } onChange={ NumeroCuentaOnChange }/>
                     <h5 className="card-subtitle mb-2 text-muted mt-4">Número de operación: </h5>
                     <div className="card">
                         <div className="card-body">
-                            <p>111111111111111111</p>
+                            <p></p>
                         </div>
                     </div>
-                    <button className="mt-4 btn btn-success btn-outline-dark" onClick={
+                    <button className="mt-4 btn btn-success btn-outline-light" onClick={
                         (() => {
                             location.href = "/Historia30"
                         })
@@ -30,14 +47,14 @@ const Card = (props) => {
                     <h3 className="card-title text-center">Compra de Bitcoins</h3>
                     <h4 className="text-center">Paso 1 de 3</h4>
                     <h5 className="card-subtitle mb-4 mt-4 text-muted">Ingrese su dirección de billetera: </h5>
-                    <input type="text" className="form-control mt-4" />
+                    <input type="text" className="form-control mt-4" defaultValue={ NumeroBilletera } onChange={ NumeroBilleteraOnChange }/>
                     <h5 className="card-subtitle mb-2 text-muted mt-4">Número de operación: </h5>
                     <div className="card">
                         <div className="card-body">
-                            <p>111111111111111111</p>
+                            <p></p>
                         </div>
                     </div>
-                    <button className="mt-4 btn btn-success btn-outline-dark" onClick={
+                    <button className="mt-4 btn btn-success btn-outline-light" onClick={
                         (() => {
                             location.href = "/Historia31"
                         })
@@ -54,13 +71,17 @@ const Card = (props) => {
                     <h5 className="card-subtitle mb-4 mt-4 text-muted">Dirección de billetera de la página: </h5>
                     <div className="card">
                         <div className="card-body">
-                            <p>12345678910</p>
+                            <p></p>
                         </div>
                     </div>
                     <h5 className="card-subtitle mb-2 text-muted mt-4">Ingrese el número de transacción: </h5>
-                    <input type="text" className="form-control mt-4" />
-
-                    <button className="mt-4 btn btn-success btn-outline-dark">Enviar</button>
+                    <input type="text" className="form-control mt-4"  defaultValue={ NumeroTransaccion } onChange={ NumeroTransaccionOnChange }/>
+                    
+                    <button className="mt-4 btn btn-success btn-outline-light" onClick={
+                        (() => {
+                            location.href = "/Historia35"
+                        })
+                    }>Enviar</button>
                 </div>
             </div>
         </aside>
@@ -73,13 +94,34 @@ const Card = (props) => {
                     <h5 className="card-subtitle mb-4 mt-4 text-muted">Número de cuenta bancaria de la página: </h5>
                     <div className="card">
                         <div className="card-body">
-                            <p>12345678910</p>
+                            <p></p>
                         </div>
                     </div>
                     <h5 className="card-subtitle mb-2 text-muted mt-4">Ingrese el número de transacción: </h5>
-                    <input type="text" className="form-control mt-4" />
+                    <input type="text" className="form-control mt-4"  defaultValue={ NumeroTransaccion } onChange={ NumeroTransaccionOnChange }/>
 
-                    <button className="mt-4 btn btn-success btn-outline-dark">Enviar</button>
+                    <button className="mt-4 btn btn-success btn-outline-light"
+                    onClick={
+                        (() => {
+                            location.href = "/Historia35"
+                        })
+                    }>Enviar</button>
+                </div>
+            </div>
+        </aside>
+    }else if (props.modo == "H35") {
+        return <aside >
+            <div className="card mt-4">
+                <div className="card-body">
+                    <h3 className="card-title text-center">Venta de Bitcoins</h3>
+                    <h4 className="text-center">Paso 3 de 3</h4>
+                    <div className="alert alert-success mt-4">Operación Realizada con éxito</div>
+                    <h5 className="card-subtitle mb-4 mt-4 text-muted">Número operación: </h5>
+                    <div className="card">
+                        <div className="card-body">
+                            <p></p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </aside>
---
+++ cryptonita/pages/Historia35.js
@@ -0,0 +1,15 @@
+import Card from "../components/Card"
+export default function Historia35() {
+    return <div class="container">
+        <div className="row">
+            <div className="col-md-2">
+            </div>
+            <div className="col-md-8">           
+            <Card modo={"H35"}/>
+            </div>
+            <div className="col-md-2">
+            </div>
+        </div>
+    </div>
+
+}
