40286bdf9e5d8b93a0d8e362eb275817172069a1
franco <franco1.alfra@gmail.com>
2022-02-16 23:33:29 -0500

    container a la app

--- cryptonita/pages/_app.js
+++ cryptonita/pages/_app.js
@@ -17,8 +17,10 @@ function MyApp({ Component, pageProps }) {
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossOrigin="anonymous"
       ></script>
-
+      <div className='container'>
       <Component {...pageProps} />
+      </div>
+      
     </>
   ;
 }
