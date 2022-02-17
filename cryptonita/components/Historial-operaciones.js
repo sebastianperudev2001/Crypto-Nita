5b991247478bde9b5c43be5e1c240eafbea47ed1
Matias Villanueva <matias.villanueva.paz@gmail.com>
2022-02-16 22:28:29 -0500

    Cambios Responsive

--- cryptonita/components/Historial-operaciones.js
+++ cryptonita/components/Historial-operaciones.js
@@ -1,31 +1,33 @@
 const HisotrialOperaciones = (props) => {
     return <main className="col-md-8">
         <h3 className="mt-4">Historial de Operaciones</h3>
-        <table className="table mt-4 table-dark">
-            <thead>
-                <tr>
-                    <th>ID de Operación</th>
-                    <th>Fecha y Hora</th>
-                    <th>Tipo de Operación</th>
-                    <th>Tipo de Cambio</th>
-                    <th>Monto</th>
-                </tr>
-            </thead>
-            <tbody id="data_operaciones">
-                {
-                    props.Operaciones.map((operacion) => {
-                        return <tr key={operacion.id}>
-                            <td>{operacion.id}</td>
-                            <td>{operacion.FechaHora}</td>
-                            <td>{operacion.Tipo}</td>
-                            <td>{operacion.TipoCamio}</td>
-                            <td>{operacion.Monto}</td>
+        <div className="table-responsive">
+            <table className="table mt-4 table-dark">
+                <thead>
+                    <tr>
+                        <th>ID de Operación</th>
+                        <th>Fecha y Hora</th>
+                        <th>Tipo de Operación</th>
+                        <th>Tipo de Cambio</th>
+                        <th>Monto</th>
+                    </tr>
+                </thead>
+                <tbody id="data_operaciones">
+                    {
+                        props.Operaciones.map((operacion) => {
+                            return <tr key={operacion.id}>
+                                <td>{operacion.id}</td>
+                                <td>{operacion.FechaHora}</td>
+                                <td>{operacion.Tipo}</td>
+                                <td>{operacion.TipoCamio}</td>
+                                <td>{operacion.Monto}</td>
 
-                        </tr>
-                    })
-                }
-            </tbody>
-        </table>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
     </main>
 }
 export default HisotrialOperaciones
