3c54472a7f0149aed3b94bde2907949f1cefb6bf
Sebastián Chavarry <69838707+sebastianperudev2001@users.noreply.github.com>
2022-03-02 18:29:50 -0500

    Ahora el token de inicioSesion es el ID de usuario en lugar del correo

--- cryptonita/pages/index.js
+++ cryptonita/pages/index.js
@@ -28,9 +28,9 @@ export default function Home() {
     if (usuarioValidar == null) {
       console.log('No existe esa vaina oe');
     } else if (username == 'admin@ulima.com' && password == '123') {
-      localStorage.setItem('iniciadoSesion', username);
+      localStorage.setItem('iniciadoSesion', usuarioValidar.id);
     } else if (contra == usuarioValidar.password) {
-      localStorage.setItem('iniciadoSesion', username);
+      localStorage.setItem('iniciadoSesion', usuarioValidar.id);
 
       if (usuarioValidar.estado == 'activo') {
         location.href = '/iniciadoSesion';
